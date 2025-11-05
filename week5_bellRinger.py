1df5ebd3a362140079c59e6ed3e7547bfecc2586
Joanthan-09 <jonymn011@gmail.com>
2025-11-05 20:55:16 +0000

    done

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -78,17 +78,43 @@ split_motto = motto_string.split('a')
 
 # Replacing Words:
 # Modify the sentence: "Life is what happens when you are busy making other plans."
-sentence ="Life is what happens when you are busy making other plans."
+sentence = "Life is what happens when you are busy making other plans."
 # a. Replace "busy" with "distracted".
+sentence = sentence.replace("busy", "distracted")
 # b. Replace "plans" with "mistakes".
+sentence = sentence.replace("plans", "mistakes")
+print(sentence)
+
 
 # Problem Set 4: String Properties and Advanced Operations
 # Repetition:
 # Concatenate the word "Iteration" 7 times.
+word = "Iteration"
+result = word * 7
+print(result)
 
 # Word Search:
 # Check if the word "moonlight" appears in the quote: "With freedom, books, flowers, and the moon, who could not be happy? - Oscar Wilde"
 
+quote = "With freedom, books, flowers, and the moon, who could not be happy? - Oscar Wilde"
+
+
+word = "moonlight"
+
+
+if word in quote:
+    print(f'The word "{word}" was found in the quote.')
+else:
+    print(f'The word "{word}" was not found in the quote.')
+
 # Length and Count:
 # a. Calculate the number of characters (including spaces and punctuation) in the word/phrase: "Supercalifragilisticexpialidocious".
 # b. Count the number of times the letter 'i' appears in the same word/phrase.
+
+phrase = "Supercalifragilisticexpialidocious"
+num_characters = len(phrase)
+count_i = phrase.lower().count('i')
+
+
+print(f"Number of characters: {num_characters}")
+print(f"Count of 'i': {count_i}")
